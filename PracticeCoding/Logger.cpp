e66452abaf8af1fbfce40b843d5034cedae19efe
solomon <solomonjohnson@live.in>
2020-12-21 17:58:48 +0530

    init

---
+++ PracticeCoding/Logger.cpp
@@ -0,0 +1,7 @@
+#include "Logger.h"
+#include <iostream>
+
+void Logger::PrintSomething(std::string data)
+{
+	std::cout << data;
+}
---
+++ PracticeCoding/Main.cpp
@@ -0,0 +1,7 @@
+#include "Logger.h"
+
+void Main()
+{
+	Logger log;
+	log.PrintSomething("Hello World");
+}
